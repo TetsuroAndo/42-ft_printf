1947f1fd8c0d8957fcf239b447e5cc2e5110c7b7
TetsuroAndo <teando@c2r7s9.42tokyo.jp>
2024-11-09 00:32:53 +0900

    edit text

--- inc/ft_printf.h
+++ inc/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: teando <teando@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 22:28:51 by teando            #+#    #+#             */
-/*   Updated: 2024/11/08 23:21:24 by teando           ###   ########.fr       */
+/*   Updated: 2024/11/09 00:31:51 by teando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,12 @@
 # include <stdint.h>
 # include <unistd.h>
 
-// ft_libft
 int	ft_putchar(int c);
 
-// ft_printf
 int	ft_printf(const char *format, ...);
-
-// type_putbase
 int	type_putbase(long nbr, char *base);
-
-// type_putchar
 int	type_putchar(int c);
-
-// type_putptr
 int	type_putptr(uintptr_t nbr, char *base);
-
-// type_putstr
 int	type_putstr(char *str);
 
 #endif
